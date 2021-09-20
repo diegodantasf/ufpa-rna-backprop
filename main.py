d77c7f961dd3bf0bbd076c4c67b6db1d9f637b1c
Jeremias Abreu <jeremias10j@gmail.com>
2021-09-20 19:35:24 -0300

    change random function

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from src.plot import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-TRAIN_DATASET_SIZE = 500
-TEST_DATASET_SIZE = 500
-VALIDATION_DATASET_SIZE = 500
+TRAIN_DATASET_SIZE = 100
+TEST_DATASET_SIZE = 100
+VALIDATION_DATASET_SIZE = 100
 N_EPOCHS = 1000
 N_NEURONS = 32
 
 FUNCTION = np.sin
-LOW = 0
-HIGH = 2 * np.pi
+LOW = -10 * np.pi
+HIGH = 10 * np.pi
 
 def main():
     train_data = Dataset(FUNCTION, lo=LOW, hi=HIGH, n=TRAIN_DATASET_SIZE, with_noise=False, seed=1337)
@@ -22,7 +22,7 @@ def main():
 
     net = RNA(N_NEURONS)
 
-    [train_loss, test_loss] = net.train(train_data.X, train_data.y, test_data.X, test_data.y, num_epochs=N_EPOCHS, lr=0.01)
+    [train_loss, test_loss] = net.train(train_data.X, train_data.y, test_data.X, test_data.y, num_epochs=N_EPOCHS, lr=0.0001)
     y_pred = net.predict(validation_data.X)
 
     plot_losses(train_loss, test_loss, filename='losses_test-loss-{}.png'.format(test_loss[-1]))
