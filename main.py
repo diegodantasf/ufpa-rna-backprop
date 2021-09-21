ae0e7bb602224b2ccaa84c6eab7d932005d8d615
Jeremias Abreu <jeremias10j@gmail.com>
2021-09-21 16:06:06 -0300

    fix bias bug

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from src.plot import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-TRAIN_DATASET_SIZE = 1000
-TEST_DATASET_SIZE = 1000
-VALIDATION_DATASET_SIZE = 1000
-N_EPOCHS = 1000
-N_NEURONS = 64
+TRAIN_DATASET_SIZE = 100
+TEST_DATASET_SIZE = 100
+VALIDATION_DATASET_SIZE = 100
+N_EPOCHS = 4000
+N_NEURONS = 16
+
+def f(x):
+    return x**2
 
 FUNCTION = np.sin
-LOW = -10 * np.pi
-HIGH = 20 * np.pi
+LOW = -5 * np.pi
+HIGH = 10 * np.pi
 
 def main():
     train_data = Dataset(FUNCTION, lo=LOW, hi=HIGH, n=TRAIN_DATASET_SIZE, with_noise=False, seed=1337)
@@ -22,8 +25,13 @@ def main():
     validation_data = Dataset(FUNCTION, lo=LOW, hi=HIGH, n=VALIDATION_DATASET_SIZE, with_noise=False, seed=333)
 
     net = Network([1, N_NEURONS, 1])
+
+    # print('weights: ', net.weights)
+    # print('biases: ', net.biases)
     
-    train_loss, test_loss = net.train(train_data.X, train_data.y, N_EPOCHS, 5, lr=0.1, test_X=test_data.X, test_y=test_data.y, cross_validation=True)
+    train_loss, test_loss = net.train(train_data.X, train_data.y, N_EPOCHS, 5, lr=0.0009, test_X=test_data.X, test_y=test_data.y, cross_validation=True)
+    # print('weights: ', net.weights)
+    # print('biases: ', net.biases)
 
     y_pred = net.predict(np.transpose(validation_data.X))
     y_pred = np.squeeze(y_pred)
--- src/network.py
+++ src/network.py
@@ -17,13 +17,14 @@ class Network(object):
   def forward(self, X):
     for b, w in zip(self.biases[:-1], self.weights[:-1]):
       X = tanh(np.dot(w, X) + b)
-    X = np.dot(self.weights[-1], X) + b[-1]
+    X = np.dot(self.weights[-1], X) + self.biases[-1]
   
     return X
   
   def evaluate(self, X, y):
     results = self.forward(X)
-    return MSE(y, results)
+    ans = MSE(y, results)
+    return ans
   
   def predict(self, X):
     return self.forward(X)
@@ -39,7 +40,6 @@ class Network(object):
   def SDG(self, train_X, train_y, epochs, mini_batch_size, lr=0.001, test_X = None, test_y = None, cross_validation=None):
     train_X = np.array(train_X)
     train_y = np.array(train_y)
-    n = len(train_y)
 
     train_losses = []
     test_losses = []
@@ -48,19 +48,12 @@ class Network(object):
       test_X = np.array(test_X)
       test_y = np.array(test_y)
     
-
-    
     train_X = np.transpose(train_X)
     test_X = np.transpose(test_X)
 
     for e in range(epochs):
-      #train_X, train_y = self.shuffle_data(train_X, train_y)
-      mini_batches_X = [train_X[:, k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
-      mini_batches_y = [train_y[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
       
-      for mini_batch_X, mini_batch_y in zip(mini_batches_X, mini_batches_y):
-        #mini_batch_X = np.transpose(mini_batch_X)
-        self.update_mini_batch(mini_batch_X, mini_batch_y, lr)
+      self.update_weights(train_X, train_y, lr)
 
       train_loss = self.evaluate(train_X, train_y)
       train_losses.append(train_loss)
@@ -78,23 +71,20 @@ class Network(object):
     
     return (np.array(train_losses), np.array(test_losses))
 
-  def update_mini_batch(self, mini_batch_X, mini_batch_y, lr):
+  def update_weights(self, data_X, data_y, lr):
 
-    new_b = [np.zeros(b.shape) for b in self.biases]
-    new_w = [np.zeros(w.shape) for w in self.weights]
-
-    delta_new_b, delta_new_w = self.backprop(mini_batch_X, mini_batch_y)
-    new_b += delta_new_b
-    new_w += delta_new_w
+    delta_new_b, delta_new_w = self.backprop(data_X, data_y)
+    new_b = delta_new_b
+    new_w = delta_new_w
 
 
     # for x, y in zip(mini_batch_X, mini_batch_y):
     #   delta_new_b, delta_new_w = self.backprop(x, y)
     #   new_b = [nb+dnb for nb, dnb in zip(new_b, delta_new_b)]
     #   new_w = [nw+dnw for nw, dnw in zip(new_w, delta_new_w)]
-    
-    self.weights = [w - (lr/len(mini_batch_y))*nw for w, nw in zip(self.weights, new_w)]
-    self.biases = [b-(lr/len(mini_batch_y))*nb for b, nb in zip(self.biases, new_b)]
+
+    self.weights = np.subtract(self.weights, np.multiply(lr, new_w))
+    self.biases = np.subtract(self.biases, np.multiply(lr, new_b))
   
   def backprop(self, x, y):
     local_grad_b = [np.zeros(b.shape) for b in self.biases]
@@ -112,24 +102,27 @@ class Network(object):
       activations.append(activation)
     
     # backward
-    delta = MSE_derivative(activations[-1], y) * tanh_prime(zs[-1])
+    delta = MSE_derivative(y, activations[-1]) * tanh_prime(zs[-1])
     local_grad_b[-1] = delta
     local_grad_w[-1] = np.dot(delta, np.transpose(activations[-2]))
 
     for l in range(2, self.num_layers):
       z = zs[-l]
-      sp = tanh_prime(z)
-      delta = np.dot(np.transpose(self.weights[-l+1]), delta) * sp
+      tp = tanh_prime(z)
+      delta = np.dot(np.transpose(self.weights[-l+1]), delta) * tp
       local_grad_b[-l] = delta
       local_grad_w[-l] = np.dot(delta, np.transpose(activations[-l-1]))
+    
+    # print('lb: ', local_grad_b)
+    # print('lw: ', local_grad_w)
     
     return (local_grad_b, local_grad_w)
 
 def MSE(y, y_hat):
     return np.mean((np.subtract(y, y_hat))**2)
 
-def MSE_derivative(activations, y):
-    return np.multiply(-2, np.subtract(activations, y))
+def MSE_derivative(y, y_hat):
+    return np.multiply(-2, np.subtract(y, y_hat))
     
 
 def tanh(z):
